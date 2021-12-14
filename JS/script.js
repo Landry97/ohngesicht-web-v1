50d0b1d7ffb69262dc9e1302468de1723a918532
Landry97 <92038899+Landry97@users.noreply.github.com>
2021-12-15 00:19:30 +0100

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+//TO TOP BUTTON
+mybutton = document.getElementById("top-btn");
+// [When the user scrolls down 20px from the top of the document, show the button]
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+     if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
+       mybutton.style.display = "flex";} 
+       else {
+       mybutton.style.display = "none";}
+   };
+// [When the user clicks on the button, scroll to the top of the document]
+function topFunction() {
+ document.body.scrollTop = 0; // For Safari
+ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
